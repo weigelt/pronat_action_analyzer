45c7ed16cb037c31a5506503249ed64567254b3a
Sebastian Weigelt <weigelt@kit.edu>
2018-03-02 16:13:39 +0100

    add metainf services

--- src/main/java/edu/kit/ipd/parse/actionRecognizer/ActionRecognizer.java
+++ src/main/java/edu/kit/ipd/parse/actionRecognizer/ActionRecognizer.java
@@ -2,66 +2,65 @@ package edu.kit.ipd.parse.actionRecognizer;
 
 import java.util.LinkedList;
 
+import org.kohsuke.MetaInfServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.kit.ipd.parse.luna.agent.AbstractAgent;
 
+@MetaInfServices(AbstractAgent.class)
 public class ActionRecognizer extends AbstractAgent {
 
-    private static final Logger logger = LoggerFactory.getLogger(ActionRecognizer.class);
-    private ActionGraph actionGraph;
+	private static final Logger logger = LoggerFactory.getLogger(ActionRecognizer.class);
+	private ActionGraph actionGraph;
 
-    @Override
-    public void init() {
-        setId("actionRecognizer");
-    }
+	@Override
+	public void init() {
+		setId("actionRecognizer");
+	}
 
-    @Override
-    protected void exec() {
-        System.out.println();
-        logger.info(
-                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Action Recognizer starts execution!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	@Override
+	protected void exec() {
+		System.out.println();
+		logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Action Recognizer starts execution!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
-        // actionGraph has the attribute role, new Predicate token type and new
-        // arcs.
-        this.actionGraph = new ActionGraph(graph);
+		// actionGraph has the attribute role, new Predicate token type and new
+		// arcs.
+		this.actionGraph = new ActionGraph(graph);
 
-        LinkedList<Action> actions = actionGraph.getActions();
-        for (int i = 0; i < actions.size(); i++) {
-            RoleIdentifier roleIdentifier = new RoleIdentifier(actionGraph, actions.get(i));
-            roleIdentifier.execute();
+		LinkedList<Action> actions = actionGraph.getActions();
+		for (int i = 0; i < actions.size(); i++) {
+			RoleIdentifier roleIdentifier = new RoleIdentifier(actionGraph, actions.get(i));
+			roleIdentifier.execute();
 
-            AndAnalyser andAnalyser = new AndAnalyser(actionGraph, actions.get(i),
-                    i == 0 ? null : actions.get(i - 1),
-                    i == actions.size() - 1 ? null : actions.get(i + 1));
-            andAnalyser.execute();
+			AndAnalyser andAnalyser = new AndAnalyser(actionGraph, actions.get(i), i == 0 ? null : actions.get(i - 1),
+					i == actions.size() - 1 ? null : actions.get(i + 1));
+			andAnalyser.execute();
 
-            actionGraph.addPredicate(actions.get(i).createPredicate());
-        }
+			actionGraph.addPredicate(actions.get(i).createPredicate());
+		}
 
-        ArcBuilder arcBuilder = new ArcBuilder(actionGraph);
-        arcBuilder.execute();
+		ArcBuilder arcBuilder = new ArcBuilder(actionGraph);
+		arcBuilder.execute();
 
-        // System.out.println();
-        // System.out.println("Results:");
-        // this.actionGraph.printTokensWithRole();
-        // this.actionGraph.printAllBetweenTokenArcs();
-        // for (Predicate p: this.actionGraph.getPredicates()) {
-        // System.out.println(p);
-        // }
-        // for(Instruction i: this.actionGraph.getInstructions()) {
-        // System.out.println(i);
-        // }
+		// System.out.println();
+		// System.out.println("Results:");
+		// this.actionGraph.printTokensWithRole();
+		// this.actionGraph.printAllBetweenTokenArcs();
+		// for (Predicate p: this.actionGraph.getPredicates()) {
+		// System.out.println(p);
+		// }
+		// for(Instruction i: this.actionGraph.getInstructions()) {
+		// System.out.println(i);
+		// }
 
-        logger.info(
-                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Action Recognizer ends execution!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        System.out.println();
+		logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Action Recognizer ends execution!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println();
 
-    }
+	}
 
-    public ActionGraph getActionGraph() {
-        return this.actionGraph;
-    }
+	public ActionGraph getActionGraph() {
+		return this.actionGraph;
+	}
 
 }
