ab4b1060073bf88e70dead4733780ce1b5744b85
Sebastian Weigelt <weigelt@kit.edu>
2021-03-11 12:18:33 +0100

    removed this and add new method that must be implemented with luna 0.6.7

--- src/main/java/edu/kit/ipd/parse/actionRecognizer/AdapterGraph.java
+++ src/main/java/edu/kit/ipd/parse/actionRecognizer/AdapterGraph.java
@@ -19,127 +19,132 @@ public class AdapterGraph implements IGraph {
 
 	@Override
 	public IGraph clone() {
-		return this.graph.clone();
+		return graph.clone();
 	}
 
 	@Override
 	public IArc createArc(INode src, INode tar, IArcType type) {
-		return this.graph.createArc(src, tar, type);
+		return graph.createArc(src, tar, type);
 	}
 
 	@Override
 	public IArcType createArcType(String name) {
-		return this.graph.createArcType(name);
+		return graph.createArcType(name);
 	}
 
 	@Override
 	public INode createNode(INodeType type) {
-		return this.graph.createNode(type);
+		return graph.createNode(type);
 	}
 
 	@Override
 	public INodeType createNodeType(String name) {
-		return this.graph.createNodeType(name);
+		return graph.createNodeType(name);
 	}
 
 	@Override
 	public void deleteArc(IArc arc) {
-		this.graph.deleteArc(arc);
+		graph.deleteArc(arc);
 	}
 
 	@Override
 	public void deleteNode(INode node) {
-		this.graph.deleteNode(node);
+		graph.deleteNode(node);
 	}
 
 	@Override
 	public IArcType getArcType(String name) {
-		return this.graph.getArcType(name);
+		return graph.getArcType(name);
 	}
 
 	@Override
 	public Set<IArcType> getArcTypes() {
-		return this.graph.getArcTypes();
+		return graph.getArcTypes();
 	}
 
 	@Override
 	public List<IArc> getArcs() {
-		return this.graph.getArcs();
+		return graph.getArcs();
 	}
 
 	@Override
 	public List<IArc> getArcsOfType(IArcType type) {
-		return this.graph.getArcsOfType(type);
+		return graph.getArcsOfType(type);
 	}
 
 	@Override
 	public INodeType getNodeType(String name) {
-		return this.graph.getNodeType(name);
+		return graph.getNodeType(name);
 	}
 
 	@Override
 	public Set<INodeType> getNodeTypes() {
-		return this.graph.getNodeTypes();
+		return graph.getNodeTypes();
 	}
 
 	@Override
 	public List<INode> getNodes() {
-		return this.graph.getNodes();
+		return graph.getNodes();
 	}
 
 	@Override
 	public List<INode> getNodesOfType(INodeType type) {
-		return this.graph.getNodesOfType(type);
+		return graph.getNodesOfType(type);
 	}
 
 	@Override
 	public void giveArcNewSource(IArc arc, INode src) {
-		this.graph.giveArcNewSource(arc, src);
+		graph.giveArcNewSource(arc, src);
 	}
 
 	@Override
 	public void giveArcNewTarget(IArc arc, INode tar) {
-		this.graph.giveArcNewTarget(arc, tar);
+		graph.giveArcNewTarget(arc, tar);
 	}
 
 	@Override
 	public boolean hasArcType(String name) {
-		return this.graph.hasArcType(name);
+		return graph.hasArcType(name);
 	}
 
 	@Override
 	public boolean hasCanged() {
-		return this.graph.hasCanged();
+		return graph.hasCanged();
+	}
+
+	@Override
+	public long getLastChangedMillis() {
+		return graph.getLastChangedMillis();
 	}
 
 	@Override
 	public boolean hasNodeType(String name) {
-		return this.graph.hasNodeType(name);
+		return graph.hasNodeType(name);
 	}
 
 	@Override
 	public void removeUnusesedNodes() {
-		this.graph.removeUnusesedNodes();
+		graph.removeUnusesedNodes();
 	}
 
 	@Override
 	public String showGraph() {
-		return this.graph.showGraph();
+		return graph.showGraph();
 	}
 
 	@Override
 	public void replaceNode(INode oldNode, INode newNode, boolean deleteArcs) {
-		this.graph.replaceNode(oldNode, newNode, deleteArcs);
+		graph.replaceNode(oldNode, newNode, deleteArcs);
 	}
 
 	@Override
 	public long getChangeCounter() {
-		return this.graph.getChangeCounter();
+		return graph.getChangeCounter();
 	}
 
 	@Override
 	public void increaseChangeCounter() {
-		this.graph.increaseChangeCounter();
+		graph.increaseChangeCounter();
 
 	}
 
